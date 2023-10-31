0acc07c2aa6ea28c1c93630a74c076ae1142a82a
yoyo53 <94387950+yoyo53@users.noreply.github.com>
2023-10-31 12:09:35 +0100

    Initial commit

---
+++ back/server.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = process.env.PORT ?? 3000
+
+app.use(cors({
+  "origin": "*",
+  "methods": "GET,PATCH,POST,PUT,DELETE,OPTIONS",
+  "allowedHeaders": "X-Requested-With,Content-Type,Authorization"
+}))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello world!')
+})
