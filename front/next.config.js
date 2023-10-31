466e0a4a8bd077a66be0fd7c5512d8ab1562ef47
yoyo53 <94387950+yoyo53@users.noreply.github.com>
2023-10-31 12:55:18 +0100

    fix front deployment

--- front/next.config.js
+++ front/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+    reactStrictMode: true,
+    output: 'export',
+    basePath: '/Y'
 }
 
 module.exports = nextConfig
