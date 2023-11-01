208e9c9d06ea7b06d200be6cef0ea215868ba5ff
yoyo53 <94387950+yoyo53@users.noreply.github.com>
2023-11-01 13:04:44 +0100

    create login and register pages

---
+++ front/src/app/login/page.js
@@ -0,0 +1,134 @@
+"use client";
+
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { redirectLogedIn } from '../../utils/security'
+
+export default function LoginPage () {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    document.querySelector('body').classList.add('h-full', 'bg-white')
+    document.querySelector('html').classList.add('h-full')
+  });
+
+  const postLogin = async (event) => {
+    event.preventDefault();
+    let response = await fetch(`${process.env.ROOTAPI}auth/login`, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: email,
+        password: password
+      }),
+      headers: {
+        'Content-type': 'application/json',
+      },
+    })
+    let jsonResponse = await response.json();
+    console.log(jsonResponse)
+    if (response.ok) {
+      window.localStorage.setItem("token", jsonResponse.token);
+      router.push("/");
+    }
+  }
+
+  const forgotPassword = () => {
+    alert("That's sad 😭")
+  }
+
+  return (
+    <>
+      <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
+        <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
+          <img
+            className='mx-auto h-10 w-auto'
+            src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
+            alt='Your Company'
+          />
+          <h2 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
+          <form className='space-y-6' method='POST' onSubmit={postLogin}>
+            <div>
+              <label
+                htmlFor='email'
+                className='block text-sm font-medium leading-6 text-gray-900'
+              >
+                Email address
+              </label>
+              <div className='mt-2'>
+                <input
+                  id='email'
+                  name='email'
+                  type='email'
+                  autoComplete='email'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className='flex items-center justify-between'>
+                <label
+                  htmlFor='password'
+                  className='block text-sm font-medium leading-6 text-gray-900'
+                >
+                  Password
+                </label>
+                <div className='text-sm'>
+                  <Link
+                    href=''
+                    onClick={forgotPassword}
+                    className='font-semibold text-indigo-600 hover:text-indigo-500'
+                  >
+                    Forgot password?
+                  </Link>
+                </div>
+              </div>
+              <div className='mt-2'>
+                <input
+                  id='password'
+                  name='password'
+                  type='password'
+                  autoComplete='current-password'
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type='submit'
+                className='flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p className='mt-10 text-center text-sm text-gray-500'>
+            Not a member?{' '}
+            <Link
+              href='/register'
+              className='font-semibold leading-6 text-indigo-600 hover:text-indigo-500'
+            >
+              Create your account for free
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ front/src/app/register/page.js
@@ -0,0 +1,172 @@
+"use client";
+
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
+export default function RegisterPage () {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    const input = document.getElementById("confirmPassword");
+    if (confirmPassword != password) {
+      input.setCustomValidity("Passwords don't match");
+    } else {
+      input.setCustomValidity('');
+    }
+  }, [confirmPassword]);
+
+
+  useEffect(() => {
+    document.querySelector('body').classList.add('h-full', 'bg-white')
+    document.querySelector('html').classList.add('h-full')
+  });
+
+  const postRegister = async (event) => {
+    event.preventDefault();
+    let response = await fetch(`${process.env.ROOTAPI}auth/register`, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: email,
+        password: password
+      }),
+      headers: {
+        'Content-type': 'application/json',
+      },
+    })
+    let jsonResponse = await response.json();
+    console.log(jsonResponse)
+    if (response.ok) {
+        router.push("/login");
+    }
+  }
+
+  return (
+    <>
+      <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
+        <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
+          <img
+            className='mx-auto h-10 w-auto'
+            src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
+            alt='Your Company'
+          />
+          <h2 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
+            Create your account
+          </h2>
+        </div>
+
+        <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
+          <form className='space-y-6' method='POST' onSubmit={postRegister}>
+            <div>
+              <label
+                htmlFor='email'
+                className='block text-sm font-medium leading-6 text-gray-900'
+              >
+                Email address
+              </label>
+              <div className='mt-2'>
+                <input
+                  id='email'
+                  name='email'
+                  type='email'
+                  autoComplete='email'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor='password'
+                className='block text-sm font-medium leading-6 text-gray-900'
+              >
+                Password
+              </label>
+              <div className='mt-2'>
+                <input
+                  id='password'
+                  name='password'
+                  type='password'
+                  autoComplete='current-password'
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor='confirmPassword'
+                className='block text-sm font-medium leading-6 text-gray-900'
+              >
+                Confirm password
+              </label>
+              <div className='mt-2'>
+                <input
+                  id='confirmPassword'
+                  name='confirmPassword'
+                  type='password'
+                  autoComplete='current-password'
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  required
+                  className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                />
+              </div>
+            </div>
+
+
+            <div>
+              <label
+                htmlFor='username'
+                className='block text-sm font-medium leading-6 text-gray-900'
+              >
+                Username
+              </label>
+              <div className='mt-2'>
+                <input
+                  id='username'
+                  name='username'
+                  type='text'
+                  autoComplete='username'
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                  className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type='submit'
+                className='flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+              >
+                Register
+              </button>
+            </div>
+          </form>
+
+          <p className='mt-10 text-center text-sm text-gray-500'>
+            Already have an account?{' '}
+            <Link
+              href='/login'
+              className='font-semibold leading-6 text-indigo-600 hover:text-indigo-500'
+            >
+              Log in
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
