8b2b03848b2f9507cee9f04145e706b629c4ab0b
yoyo53 <94387950+yoyo53@users.noreply.github.com>
2023-11-02 16:55:39 +0100

    fix favicon

--- front/src/app/layout.js
+++ front/src/app/layout.js
@@ -14,6 +14,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="./favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png"/>
+      </head>
       <body className={'flex flex-col min-h-screen bg-white ' + inter.className}>
         <Toaster />
         <Header />
