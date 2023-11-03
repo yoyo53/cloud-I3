d4594214ccf256d9f48706e5b152bf21ee1d9cfd
yoyo53 <94387950+yoyo53@users.noreply.github.com>
2023-11-03 12:46:37 +0100

    fix images on static export

--- front/next.config.js
+++ front/next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
     output: 'export',
     basePath: process.env.NODE_ENV == 'production' ? '/Y' : '',
     env: {
-        ROOTAPI: 'https://y-back.fly.dev'
+        ROOTAPI: 'https://y-back.fly.dev',
+        BASE_PATH: process.env.NODE_ENV == 'production' ? '/Y' : ''
     }
 }
 
